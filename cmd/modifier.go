78709845bd2679da6479850f47106688dedae9fb
David Rud Pedersen <davidrudpedersen@gmail.com>
2023-05-11 14:07:13 +0200

    Make create modifier command

---
+++ cmd/modifier.go
@@ -0,0 +1,53 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"log"
+	fm "revolution/filemanage"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// modifierCmd represents the modifier command
+var modifierCmd = &cobra.Command{
+	Use:   "modifier",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Args: cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+
+		outDir := viper.GetString("component_directory")
+
+		if outDir == "" {
+			log.Fatalln("Component directory is unspecified.")
+		}
+
+		if err := fm.CreateModifier(args[0], outDir); err != nil {
+			return err
+		}
+
+		return nil
+	},
+}
+
+func init() {
+	createCmd.AddCommand(modifierCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// modifierCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// modifierCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
