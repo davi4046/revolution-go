45e8fef842250f48d5445c41dd82b2e51c7bf638
David Rud Pedersen <davidrudpedersen@gmail.com>
2023-05-11 11:10:42 +0200

    Replace toKebabCase with toSnakeCase

--- filemanage/to_snake_case.go
+++ filemanage/to_snake_case.go
@@ -5,10 +5,10 @@ import (
 	"strings"
 )
 
-func toKebabCase(s string) string {
+func toSnakeCase(s string) string {
 	var matchFirstCap = regexp.MustCompile("(.)([A-Z][a-z]+)")
 	var matchAllCap = regexp.MustCompile("([a-z0-9])([A-Z])")
-	snake := matchFirstCap.ReplaceAllString(s, "${1}-${2}")
-	snake = matchAllCap.ReplaceAllString(snake, "${1}-${2}")
+	snake := matchFirstCap.ReplaceAllString(s, "${1}_${2}")
+	snake = matchAllCap.ReplaceAllString(snake, "${1}_${2}")
 	return strings.ToLower(snake)
 }
