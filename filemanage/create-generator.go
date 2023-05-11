1b9bc1a48f80eeed3fffd1198dfd751cad2d78b3
David Rud Pedersen <davidrudpedersen@gmail.com>
2023-05-11 10:43:27 +0200

    Move toKebabCase function to seperate file

--- filemanage/create-generator.go
+++ filemanage/create-generator.go
@@ -4,33 +4,31 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"regexp"
-	"strings"
 )
 
-var data = []byte(
-	`package components
-
-import "components/types"
-
-type Generator struct {
-	/*
-		Any variables declared here
-		will automatically be exposed
-		as parameters of the generator.
-	*/
-}
-
-func (g Generator) Generate(i int) types.Note {
-
-	return types.Note{
-		Midinote: 60,
-		Duration: 1,
-	}
-}
-`)
-
 func CreateGenerator(name, outDir string) error {
+
+	var data = []byte(
+		`package components
+
+	import "components/types"
+
+	type Generator struct {
+		/*
+			Any variables declared here
+			will automatically be exposed
+			as parameters of the generator.
+		*/
+	}
+
+	func (g Generator) Generate(i int) types.Note {
+
+		return types.Note{
+			Midinote: 60,
+			Duration: 1,
+		}
+	}
+	`)
 
 	fileName := filepath.Join(outDir, toKebabCase(name)+".gen.go")
 
@@ -43,13 +41,4 @@ func CreateGenerator(name, outDir string) error {
 	}
 
 	return nil
-}
-
-var matchFirstCap = regexp.MustCompile("(.)([A-Z][a-z]+)")
-var matchAllCap = regexp.MustCompile("([a-z0-9])([A-Z])")
-
-func toKebabCase(s string) string {
-	snake := matchFirstCap.ReplaceAllString(s, "${1}-${2}")
-	snake = matchAllCap.ReplaceAllString(snake, "${1}-${2}")
-	return strings.ToLower(snake)
 }
---
+++ filemanage/to-kebab-case.go
@@ -0,0 +1,14 @@
+package filemanage
+
+import (
+	"regexp"
+	"strings"
+)
+
+func toKebabCase(s string) string {
+	var matchFirstCap = regexp.MustCompile("(.)([A-Z][a-z]+)")
+	var matchAllCap = regexp.MustCompile("([a-z0-9])([A-Z])")
+	snake := matchFirstCap.ReplaceAllString(s, "${1}-${2}")
+	snake = matchAllCap.ReplaceAllString(snake, "${1}-${2}")
+	return strings.ToLower(snake)
+}
