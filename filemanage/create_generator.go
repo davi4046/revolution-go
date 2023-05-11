3d85b03257acb7a4125bcf3b8d018c3f89929102
David Rud Pedersen <davidrudpedersen@gmail.com>
2023-05-11 11:25:59 +0200

    Update create_generator.go

--- filemanage/create_generator.go
+++ filemanage/create_generator.go
@@ -4,39 +4,42 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
+
+	"github.com/iancoleman/strcase"
 )
 
 func CreateGenerator(name, outDir string) error {
 
-	var data = []byte(
-		`package components
+	var data = `package components
 
-	import "components/types"
+import "components/types"
 
-	type Generator struct {
-		/*
-			Any variables declared here
-			will automatically be exposed
-			as parameters of the generator.
-		*/
+type Generator struct {
+	/*
+		Any variables declared here
+		will automatically be exposed
+		as parameters of Generator.
+	*/
+}
+
+func (g Generator) Generate(i int) types.Note {
+
+	return types.Note{
+		Midinote: 60,
+		Duration: 1,
 	}
+}
+`
+	data = strings.ReplaceAll(data, "Generator", strcase.ToCamel(name))
 
-	func (g Generator) Generate(i int) types.Note {
-
-		return types.Note{
-			Midinote: 60,
-			Duration: 1,
-		}
-	}
-	`)
-
-	fileName := filepath.Join(outDir, toKebabCase(name)+".gen.go")
+	fileName := filepath.Join(outDir, strcase.ToSnake(name)+".gen.go")
 
 	if _, err := os.Stat(fileName); !os.IsNotExist(err) {
 		log.Fatalln("Failed to create generator: A generator by the same name already exists.")
 	}
 
-	if err := os.WriteFile(fileName, data, 0777); err != nil {
+	if err := os.WriteFile(fileName, []byte(data), 0777); err != nil {
 		return err
 	}
 
