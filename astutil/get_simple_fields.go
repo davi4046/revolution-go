0ad071ecde647d1f052a7aa6bd63687f96d81eb8
David Rud Pedersen <davidrudpedersen@gmail.com>
2023-06-01 11:40:08 +0200

    Update get_simple_fields.go

--- astutil/get_simple_fields.go
+++ astutil/get_simple_fields.go
@@ -1,26 +1,26 @@
 package astutil
 
-import "go/ast"
+import (
+	"go/ast"
+	"go/types"
+)
 
 func GetSimpleFields(fields []*ast.Field) []SimpleField {
 
 	var simpleFields []SimpleField
 
 	for _, field := range fields {
-		typeIdent, ok := field.Type.(*ast.Ident)
-		if !ok {
-			continue
-		}
+		fieldType := types.ExprString(field.Type)
 		if len(field.Names) == 0 {
 			simpleFields = append(simpleFields, SimpleField{
 				Name: "",
-				Type: typeIdent.Name,
+				Type: fieldType,
 			})
 		} else {
 			for _, nameIdent := range field.Names {
 				simpleFields = append(simpleFields, SimpleField{
 					Name: nameIdent.Name,
-					Type: typeIdent.Name,
+					Type: fieldType,
 				})
 			}
 		}
