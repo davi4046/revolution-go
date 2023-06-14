eb205ac297d535880ff6009227a8e63e8691a9af
David Rud Pedersen <davidrudpedersen@gmail.com>
2023-06-14 10:26:04 +0200

    Update component boilerplate

--- component/boilerplate/modifier/revocomp.go
+++ component/boilerplate/modifier/revocomp.go
@@ -1,49 +1,13 @@
 package main
 
-type modifier struct {
-}
+import "github.com/davi4046/revoutil"
 
-func newModifier(
+type modifier struct{}
 
-/************* Parameters **************
-
-	Any parameters declared here
-	will automatically be exposed
-	as parameters of the generator.
-
-***************************************/
-
-) modifier {
-
-	/*********** Initialization ************
-
-		Perform any one-time operations
-		here and save the result in the
-		generator struct.
-
-		In this case there are no one-
-		time operations and we simply
-		parse the parameters as is.
-
-	***************************************/
-
+func newModifier() modifier {
 	return modifier{}
 }
 
-func (m modifier) modify(in []struct {
-	pitch    int
-	duration float64
-}) []struct {
-	pitch    int
-	duration float64
-} {
-
-	/************* Generation **************
-
-		Implement the per-note generation
-		logic here. Try to keep it fairly
-		lightweight to ensure performance.
-
-	***************************************/
-	return in
+func (m modifier) modify(note revoutil.Note) []revoutil.Note {
+	return []revoutil.Note{note}
 }
